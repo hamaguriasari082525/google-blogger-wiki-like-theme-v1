db7badc32e63b951e02db47c1a8f231e970050b5
Hogehoge-a11y <asarigames0825@gmail.com>
2026-09-22 05:53:35 +0900

    Create reference-navigation-binding.js

---
+++ js/reference-navigation-binding.js
@@ -0,0 +1,65 @@
+// reference-navigation-binding.js
+
+(function(){
+"use strict";
+
+function buildReferenceNavigation(){
+  const core=window.articleReferenceCore;
+  const navigation=window.articleReferenceNavigation;
+
+  if(!core||!navigation){
+    return;
+  }
+
+  const references=core.getReferences();
+
+  references.forEach(function(reference){
+    if(
+      !reference||
+      !reference.element||
+      !reference.target||
+      !reference.target.element
+    ){
+      return;
+    }
+
+    const link=
+      reference.element.querySelector(
+        ":scope > .article-reference-link"
+      );
+
+    if(!link){
+      return;
+    }
+
+    if(link.dataset.referenceNavigationApplied==="true"){
+      return;
+    }
+
+    link.addEventListener(
+      "click",
+      function(event){
+        event.preventDefault();
+
+        const display=
+          reference.target.element.querySelector(
+            ".article-reference-display"
+          );
+
+        if(!display){
+          return;
+        }
+
+        navigation.navigate(display);
+      }
+    );
+
+    link.dataset.referenceNavigationApplied="true";
+  });
+}
+
+window.articleReferenceNavigationBinding={
+  process:buildReferenceNavigation
+};
+
+})();
